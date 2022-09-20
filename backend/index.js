7adb819d29ee7fa955aae474673fe771a92976e1
art3mis145 <art3mis145@gmail.com>
2022-09-20 16:39:14 +0700

    adding cors

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,19 @@ const express = require("express");
 const app = express();
 
 const http = require("http");
-const server = http.Server(app);
+const server = http.createServer(app);
 
-const socketIO = require("socket.io");
-const io = socketIO(server);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+  },
+});
 
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
   console.log("server running on port ", port);
+  io.on("connection", (socket) => {
+    console.log(socket.id);
+  });
 });
-
-// io.on("connection", (socket) => {
-//   socket.on("join", (data) => {
-//     socket.join(data.room);
-//     socket.broadcast.to(data.room).emit("new user join");
-//   });
-// });
